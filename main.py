131fcad06ba5013074e5d2cf3bad908304d3480f
hariprasad-renganathan <HariPrasad.Renganathan@take2games.com>
2023-05-12 19:08:01 -0400

    added extract_jobinfo func

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from src.cv_to_text import cv_to_text
 from src.scrape_linkedin import scrape_linkedin
 from src.match_percentage import match_percentage
 import shutil
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 app = FastAPI()
 
@@ -27,3 +30,35 @@ async def hello(job_title: str = Form(...), location: str = Form(...), file: Upl
     final_json = final_info_table.to_dict(orient="records")
         
     return final_json
+
+@app.post("/extract_jobinfo")
+async def hello(url: str = Form(...)):
+    headers = {'User-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    job_description = soup.find('div', id='job-details').text
+
+    image_raw_link = soup.find('div', class_='top-card-layout__card relative p-2 papabear:p-details-container-padding').findAll('img', class_="artdeco-entity-image")[0]['data-delayed-url']
+    image_link = image_raw_link.replace('amp;','')
+
+    location = soup.find('div', class_='topcard__flavor-row').findAll('span',class_='topcard__flavor topcard__flavor--bullet')[0].text.replace("\n","").strip()
+
+    company_name = soup.find('div', class_='topcard__flavor-row').find('a',class_='topcard__org-name-link topcard__flavor--black-link').text.replace("\n","").strip()
+
+    job_title = soup.find('h1', class_ = 'top-card-layout__title font-sans text-lg papabear:text-xl font-bold leading-open text-color-text mb-0 topcard__title').text
+
+    job_link = url
+
+    job_details = pd.DataFrame(columns = ['company_name', 'job_title', 'company_logo', 'job_description', 'location', 'job_link'])
+    job_details['company_name'] = [company_name]
+    job_details['job_title'] = [job_title]
+    job_details['company_logo'] = [image_link]
+    job_details['job_description'] = [job_description]
+    job_details['location'] = [location]
+    job_details['job_link'] = [job_link]
+
+    job_details = job_details.to_dict(orient="records")
+        
+    return job_details
